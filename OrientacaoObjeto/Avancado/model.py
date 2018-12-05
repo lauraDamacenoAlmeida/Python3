c351eaa5a93b3c45150a48e54ff1c89151f9d0bb
Laura Damaceno de Almeida <laura.damaceno@ibm.com>
2018-12-05 14:36:26 -0200

    list class, str

--- OrientacaoObjeto/Avancado/model.py
+++ OrientacaoObjeto/Avancado/model.py
@@ -16,7 +16,11 @@ class Programa:
         self._nome = nome.title()
 
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
+
+    def __str__(self):
+        return f'{sel._nome} - {self.ano} : {self._likes}'
+
 #Como é herança, pssamos o nome da classe mae como parametro na criacao de outra classe
 #para acessar atributos e metodos da classe mae, precisamos usar o algoritmo "super()"
 class Filme(Programa):
@@ -29,6 +33,11 @@ class Filme(Programa):
     def duracao(self):
         return self.__duracao
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min: {self._likes}'
+
+#str é usado para imprimir o nosso objeto em forma textual, na hora de testa-lo no console, só imorimir o nome da minha
+# variavel/lista
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
@@ -38,7 +47,20 @@ class Serie(Programa):
     def temporadas(self):
         return self.__temporadas
 
-    filmes_e_series = ['Got',]
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas: {self._likes}'
+
+#como a minha playlist nao é iterable, para facilitar o seu manuseio fazemos com que ela herde da classe List,
+# onde ela terá todos os componentes de uma lista. Pegamos o metodo __init__ e passamos como parametro a lista que receberemos
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
+
+
 
 
 
